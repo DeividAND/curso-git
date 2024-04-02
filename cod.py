5aefdf1692185974c7e6212a44a33e308a397723
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-04-02 09:48:16 -0300

    9° commit - modificando o cod.py

--- cod.py
+++ cod.py
@@ -1,3 +1,8 @@
 print("Hello, world!")
 nome = str(input('Digite seu nome: '))
+idade = int(input('Informe sua idade: '))
 print(f'seja bem vindo {nome}')
+if idade >= 18:
+    print(f'{nome} tem {idade} e está autorizado a dirigir.')
+else:
+    print(f'{nome} tem {idade} e não está autorizado a dirigir.')
