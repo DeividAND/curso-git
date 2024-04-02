40c49a0e7922593d975df118b6293006602aa7ad
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-04-02 09:56:06 -0300

    11° commit - encrementando o código

--- cod.py
+++ cod.py
@@ -1,8 +1,9 @@
 print("Hello, world!")
 nome = str(input('Digite seu nome: '))
 idade = int(input('Informe sua idade: '))
+hab = str(input('Você tem habilitação [S]/[N]? ')).upper().strip()
 print(f'seja bem vindo {nome}')
-if idade >= 18:
+if idade >= 18 and hab == 'S':
     print(f'{nome} tem {idade} e está autorizado a dirigir.')
 else:
     print(f'{nome} tem {idade} e não está autorizado a dirigir.')
